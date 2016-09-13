b8d3366c99fb2d703ee5a9c0c6cb03ad64efe730
iPad TechSpot <mehedi.srk2@gmail.com>
2016-09-13 19:24:39 +0600

    Update server_api.go

--- server/server_api.go
+++ server/server_api.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/anacrolix/torrent/metainfo"
 
-	"github.com/Mehedidec/mh-torrent/engine"
+	"github.com/ipadtechspot/iTS-torrent/engine"
 )
 
 func (s *Server) api(r *http.Request) error {
